e8b89835f4de442cfa00c76bc288e7f98df711f0
Khang2701 <lackhangnguyentran99@gmail.com>
2021-05-19 00:30:16 +0700

    Update Role, Validate, Navigator
    Them Show/Hide Password, Thay doi icon phone.

---
+++ client/lib/Services/auth_service.dart
@@ -0,0 +1,47 @@
+import 'package:client/Models/server.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+//import 'package:shared_preferences/shared_preferences.dart';
+
+
+class AuthService{
+  static Future<String> signupService(name, email, phone, password, role) async {
+    String apiUrl = '$URL_SIGNUP';
+    http.Response response = await http.post(apiUrl, body: {
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'password': password,
+      'role': "User" 
+    });
+    if (response.statusCode == 200) {
+      print("Result: ${response.body}");
+    } else {
+      print('dang ky that bai');
+    }
+    return response.body;
+  }
+
+  static Future<String> signInService(phone, password, role) async {
+    //SharedPreferences prefs = await SharedPreferences.getInstance();
+    String apiUrl = '$URL_SIGNIN';
+   
+    http.Response response =
+        await http.post(apiUrl, body: {'phone': phone, 'password': password, 'role': "User"});
+    if(response.statusCode == 200) {
+      /*var jsonResponseToken = json.decode(response.body);
+      prefs.setString("token", jsonResponseToken['token']);
+      var jsonResponseId = json.decode(response.body);
+      prefs.setString("id", jsonResponseId['_id']);
+      print('danh nhap thanh cong');
+      print(jsonResponseToken);
+      print(jsonResponseId);*/
+      print("Result: ${response.body}");
+      print('dang nhap thanh cong');
+    } else {
+      print('dang nhap that bai');
+    }
+    return response.body;
+  }
+}
